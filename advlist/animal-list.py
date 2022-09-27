6d3c465baecb77bd2363b8127617842b9a1e1f21
rld9389 <rld9389@gmail.com>
2022-09-27 13:55:39 +0000

    learning about dictionaries

---
+++ advlist/animal-list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def main():
+
+    #create a animal list
+    animallist = ["cat", "dog", "horse", "cow", "sheep"]
+
+    #print animal list
+    print(animallist)
+
+main()
